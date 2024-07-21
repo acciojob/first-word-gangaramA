ba03889e41fcfa4419df2df908c6ad84c72a6b8b
gangaramA <150507677+gangaramA@users.noreply.github.com>
2024-07-21 13:33:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstWord(s) {
-  // your code here
+    if (s === '') {
+        return '';
+    }
+    
+    const firstSpaceIndex = s.indexOf(' ');
+    
+    if (firstSpaceIndex === -1) {
+        return s;
+    }
+    
+    return s.substring(0, firstSpaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
